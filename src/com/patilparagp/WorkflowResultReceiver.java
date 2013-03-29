7a70d814f3f422fd391caec6d37e1dfbe52c046f
mailparaghere <mailparaghere@gmail.com>
2013-03-29 09:10:57 +0000

    Remove the Progress dialog when the results start coming in

--- src/com/patilparagp/WorkflowResultReceiver.java
+++ src/com/patilparagp/WorkflowResultReceiver.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 import android.os.ResultReceiver;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import com.patilparagp.R;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,24 +30,16 @@ public class WorkflowResultReceiver extends ResultReceiver {
     protected void onReceiveResult(int resultCode, Bundle resultData) {
         System.out.println(" ********* WorkflowResultReceiver.onReceiveResult");
         if (workflowListView != null) {
+            progressDialog.dismiss();
             workflowNames.addAll(resultData.getStringArrayList("workflowNames"));
             workflowListView.setAdapter(new ArrayAdapter<String>(context, R.layout.listrow, workflowNames));
         }
-        if(resultCode == 2) {
-            progressDialog.dismiss();
-        }
 
     }
 
-    public ListView getWorkflowListView() {
-        return workflowListView;
-    }
 
     public void setWorkflowListView(ListView workflowListView) {
         this.workflowListView = workflowListView;
-    }
-    public Context getContext() {
-        return context;
     }
 
     public void setContext(Context context) {
